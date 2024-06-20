74c54b996e1399234d9c389801021c69c9e88177
Graham Hall <graham@ghall.dev>
2024-06-20 19:40:20 -0400

    remove pagination

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ghall.blog',
-	output: 'server',
+	output: 'hybrid',
 	adapter: netlify(),
 	integrations: [
 		mdx(),
