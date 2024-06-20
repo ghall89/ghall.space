1eba3f121b19f8ed861dcf0e5d90a837e894150e
Graham Hall <graham@ghall.dev>
2024-06-20 19:36:42 -0400

    maybe fix archive page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ghall.blog',
-	output: 'hybrid',
+	output: 'server',
 	adapter: netlify(),
 	integrations: [
 		mdx(),
