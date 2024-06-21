98c1a85f21c2ee48d41a01934021441c7642e607
Graham Hall <graham@ghall.dev>
2024-06-21 15:57:11 -0400

    add carbon rating

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,10 @@
 import { z, defineCollection } from 'astro:content';
-import { Tag } from 'src/types';
 
 const blogCollection = defineCollection({
 	schema: z.object({
 		title: z.string(),
 		pubDate: z.string().transform((str: string) => new Date(str)),
-		tags: z.array(z.nativeEnum(Tag)),
+		tags: z.array(z.string()),
 	}),
 });
 
