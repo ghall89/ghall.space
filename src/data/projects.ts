b108762c601c2984f15ceb9992bfb3a85309bfbd
Graham Hall <graham@ghall.dev>
2025-09-14 14:11:26 -0400

    new projects page

---
+++ src/data/projects.ts
@@ -0,0 +1,34 @@
+import autodockImg from '@assets/projects/autodock.png';
+import keystashImg from '@assets/projects/keystash.png';
+import bggClientImg from '@assets/projects/bgg-client.png';
+
+export interface Project {
+	title: string;
+	description: string;
+	image: ImageMetadata;
+	link: string;
+}
+
+export const projects: Project[] = [
+	{
+		title: 'AutoDock',
+		description:
+			'A MacOS menubar utility for automatically hiding and showing the Dock based on the screen size of the connected displays.',
+		image: autodockImg,
+		link: 'https://github.com/ghall89/AutoDock',
+	},
+	{
+		title: 'KeyStash',
+		description:
+			'A MacOS application for managing software license keys for software purchased outside the Mac App Store.',
+		image: keystashImg,
+		link: 'https://github.com/ghall89/KeyStash',
+	},
+	{
+		title: 'bgg-client',
+		description:
+			'A TypeScript client for working with the BoardGameGeek.com API.',
+		image: bggClientImg,
+		link: 'https://www.npmjs.com/package/bgg-client',
+	},
+];
