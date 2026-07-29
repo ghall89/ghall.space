17441cce7d29a4200610b67bd7be1600baac576b
Graham Hall <graham@ghall.dev>
2026-07-29 15:27:25 -0400

    blog design adjustments + home page

---
+++ src/data/about-me.ts
@@ -0,0 +1,24 @@
+export const aboutMe = {
+	likes: [
+		'Board Games',
+		'Cats',
+		'Coffee',
+		'Computers',
+		'Dogs',
+		'Equal Rights',
+		'Fun',
+		'Music',
+		'Pizza',
+		'Trees',
+		'Video Games',
+		'Writing',
+	],
+	dislikes: [
+		'Bigotry',
+		'Generative AI',
+		'Humidity',
+		'Loud People',
+		'Raisins',
+		'Social Media',
+	],
+};
